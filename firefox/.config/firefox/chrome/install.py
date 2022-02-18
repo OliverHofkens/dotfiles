82aef4e22f914d4eb381526aa85362b76396540d
Oliver Hofkens <oliver@gorilla.co>
2022-02-18 10:52:24 +0100

    Firefox; Update install script.

--- firefox/.config/firefox/chrome/install.py
+++ firefox/.config/firefox/chrome/install.py
@@ -5,13 +5,19 @@ from pathlib import Path
 PROFILE_DIRS = {
     "Linux": "~/.mozilla/firefox/",
     "Darwin": "~/Library/Application Support/Firefox/Profiles/",
-    "Windows": "~\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\"
+    "Windows": "~\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\",
 }
 
 profile_dir = Path(PROFILE_DIRS[platform.system()]).expanduser().resolve()
-default_profile_dir = next(d for d in profile_dir.iterdir() if d.is_dir() and d.name.endswith(".default"))
+default_profile_dir = next(
+    d
+    for d in profile_dir.iterdir()
+    if d.is_dir() and d.name.endswith(".default-release")
+)
 
-userchrome_target = default_profile_dir / "chrome" / "userChrome.css"
+userchrome_dir = default_profile_dir / "chrome"
+userchrome_dir.mkdir(exist_ok=True)
+userchrome_target = userchrome_dir / "userChrome.css"
 userchrome_source = (Path(__file__).parent / "userChrome.css").resolve()
 print(f"Symlinking {userchrome_target} to {userchrome_source}")
 userchrome_target.symlink_to(userchrome_source)
