43b34dcd9e3849a1ac513c5e464826c37198154b
Oliver Hofkens <oliver@gorilla.co>
2022-05-04 11:51:05 +0200

    Fish; Fresh config.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,13 +1,5 @@
-alias pip3='python3 -m pip'
-alias ls="lsd"
+fish_add_path ~/.cargo/bin
 
-status is-interactive; and pyenv init --path | source
-
-set PATH $HOME/.cargo/bin $PATH
-direnv hook fish | source
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/ncurses/bin" $fish_user_paths
-# Created by `userpath` on 2020-02-28 07:28:32
-set PATH $PATH /Users/oliver/.local/bin
-
-starship init fish | source
+if status is-interactive
+    starship init fish | source
+end
