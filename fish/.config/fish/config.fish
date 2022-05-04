c7fe57116d6edbc7885c03666f88f2fdd0df255e
Oliver Hofkens <oliver@gorilla.co>
2022-05-04 17:28:41 +0200

    Fish; Update config.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,10 @@
 fish_add_path ~/.cargo/bin
-fish_add_path ~/Library/Python/3.9/bin
+# [a]ppend Python3.9 binaries to the end, so `pyenv` shims can take precedence.
+fish_add_path -a ~/Library/Python/3.9/bin
+fish_add_path -a ~/.local/bin
 
 pyenv init - | source
+direnv hook fish | source
 
 if status is-interactive
     starship init fish | source
