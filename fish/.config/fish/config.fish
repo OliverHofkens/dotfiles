c29213980091231f3c3ebb62ddc82dd7d34d911f
Oliver Hofkens <oliver@gorilla.co>
2022-05-27 09:13:46 +0200

    Update Brewfile, fish.config and gitconfig.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,6 +6,11 @@ fish_add_path -a ~/.local/bin
 pyenv init - | source
 direnv hook fish | source
 
+set -gx EDITOR nvim
+
 if status is-interactive
     starship init fish | source
 end
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
