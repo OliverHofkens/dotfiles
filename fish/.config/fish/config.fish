76314afb0bbc7fcb14738db8a0a0fc9f2a1209c3
Oliver Hofkens <23633993+OliverHofkens@users.noreply.github.com>
2024-11-26 14:37:35 +0100

    feat(fish): set $EDITOR to nvim

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,16 +1,17 @@
 fish_add_path ~/.cargo/bin
 # [a]ppend Python3.9 binaries to the end, so `pyenv` shims can take precedence.
 fish_add_path -a ~/Library/Python/3.9/bin
+fish_add_path -a ~/Library/Python/3.10/bin
+fish_add_path -a ~/Library/Python/3.11/bin
 fish_add_path -a ~/.local/bin
 
 pyenv init - | source
 direnv hook fish | source
 
-set -gx EDITOR hx
+set -gx EDITOR nvim
 
 if status is-interactive
     starship init fish | source
 end
 
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-
+test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
