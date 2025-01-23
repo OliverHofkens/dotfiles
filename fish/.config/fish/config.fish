26a20296ece6e4c6b5b211ffd231717c2af36707
Oliver Hofkens <23633993+OliverHofkens@users.noreply.github.com>
2025-01-23 10:54:59 +0100

    chore: cleanup

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,11 +1,6 @@
 fish_add_path ~/.cargo/bin
-# [a]ppend Python3.9 binaries to the end, so `pyenv` shims can take precedence.
-fish_add_path -a ~/Library/Python/3.9/bin
-fish_add_path -a ~/Library/Python/3.10/bin
-fish_add_path -a ~/Library/Python/3.11/bin
 fish_add_path -a ~/.local/bin
 
-pyenv init - | source
 direnv hook fish | source
 
 set -gx EDITOR nvim
