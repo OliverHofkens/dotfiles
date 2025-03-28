d82ef337969b22a628e0fdabb3b6d1e98bd9e559
Oliver Hofkens <23633993+OliverHofkens@users.noreply.github.com>
2025-03-28 14:24:47 +0100

    feat: dump and apply more settings

--- manage.py
+++ manage.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import json
 import subprocess
 from argparse import ArgumentParser, Namespace
@@ -20,6 +19,9 @@ def get_argparser() -> ArgumentParser:
     )
     parser_dump.set_defaults(func=cmd_dump)
 
+    parser_apply = subparsers.add_parser("apply", help="Apply dumped settings")
+    parser_apply.set_defaults(func=cmd_apply)
+
     return parser
 
 
@@ -37,10 +39,27 @@ def cmd_dump(args: Namespace):
     subprocess.check_output(["brew", "bundle", "dump", "--describe", "--force"])
 
 
+def cmd_apply(args):
+    print("Applying System Settings")
+    defaults = read_osx_defaults()
+    apply_osx_defaults(defaults)
+
+    # print("Installing Brewfile")
+    # subprocess.check_output(["brew", "bundle", "install"])
+
+
 @dataclass
 class DefaultValue:
     dtype: str
     value: str
+
+    def to_args(self) -> tuple[str, str]:
+        if self.dtype == "boolean":
+            val = "true" if self.value == "1" else "false"
+        else:
+            val = self.value
+
+        return ("-" + self.dtype, val)
 
 
 @dataclass
@@ -70,15 +89,18 @@ def read_osx_defaults(from_system: bool = False) -> OsxDefaults:
     if from_system:
         for ns, keys in defaults.dump.items():
             for key in keys:
-                dtype = subprocess.check_output(
-                    ["defaults", "read-type", ns, key], text=True
-                )
-                dtype = dtype.removeprefix("Type is ").strip()
-                val = subprocess.check_output(
-                    ["defaults", "read", ns, key], text=True
-                ).strip()
+                try:
+                    dtype = subprocess.check_output(
+                        ["defaults", "read-type", ns, key], text=True
+                    )
+                    dtype = dtype.removeprefix("Type is ").strip()
+                    val = subprocess.check_output(
+                        ["defaults", "read", ns, key], text=True
+                    ).strip()
 
-                defaults.values[ns][key] = DefaultValue(dtype, val)
+                    defaults.values[ns][key] = DefaultValue(dtype, val)
+                except subprocess.CalledProcessError as e:
+                    print(e.output)
 
     return defaults
 
@@ -87,6 +109,12 @@ def write_osx_defaults(defaults: OsxDefaults):
     with PATH_DEFAULTS.open("w") as f:
         data = asdict(defaults)
         json.dump(data, f, indent=2)
+
+
+def apply_osx_defaults(defaults: OsxDefaults):
+    for ns, keys in defaults.values.items():
+        for key, val in keys.items():
+            subprocess.check_output(["defaults", "write", ns, key, *val.to_args()])
 
 
 if __name__ == "__main__":
