be76aa9ba8c2f27f7459cb7bd3689d51f8ed7ef9
Oliver Hofkens <23633993+OliverHofkens@users.noreply.github.com>
2025-06-24 13:35:49 +0200

    fix(sketchybar): pick more relevant fs

--- sketchybar/.config/sketchybar/plugins/disk.sh
+++ sketchybar/.config/sketchybar/plugins/disk.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sketchybar --set "$NAME" icon="󰋊" label="$(df -H | grep -E '^(/dev/disk3s1).' | awk '{ printf ("%s\n", $5) }')"
+sketchybar --set "$NAME" icon="󰋊" label="$(df -H | grep -E '^(/dev/disk3s5).' | awk '{ printf ("%s\n", $5) }')"
