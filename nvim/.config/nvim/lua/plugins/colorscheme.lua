39cd2f850ced43483d439d6c3b6b9299dd6d8560
Oliver Hofkens <23633993+OliverHofkens@users.noreply.github.com>
2025-07-30 16:54:53 +0200

    feat(nvim): dim inactive windows in Sonokai

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -5,6 +5,7 @@ return {
   config = function()
     vim.g.sonokai_enable_italic = true
     vim.g.sonokai_transparent_background = true
+    vim.g.sonokai_dim_inactive_windows = true
     vim.cmd.colorscheme("sonokai")
   end,
 }
