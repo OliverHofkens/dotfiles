66ff03ba902ea2b0d48d8cce36fb773d3fd38a22
Oliver Hofkens <23633993+OliverHofkens@users.noreply.github.com>
2025-11-18 08:53:49 +0100

    style: reformat wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,22 +1,28 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
 -- System
 config.check_for_updates = false
-config.max_fps = 120  -- Smoother scrolling in editors
+config.max_fps = 120 -- Smoother scrolling in editors
 
 -- GUI
-config.color_scheme = 'flexoki-dark'
-config.font = wezterm.font 'Inconsolata Nerd Font'
+config.color_scheme = "flexoki-dark"
+config.font = wezterm.font("Inconsolata Nerd Font")
 config.font_size = 16
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 config.window_padding = {
-  left = '2cell',
-  right = '2cell',
-  top = '1cell',
-  bottom = '1cell',
+	left = "2cell",
+	right = "2cell",
+	top = "1cell",
+	bottom = "1cell",
+}
+
+-- Keybinds
+config.keys = {
+	-- Claude Code: newline on shift-enter
+	{ key = "Enter", mods = "SHIFT", action = wezterm.action({ SendString = "\x1b\r" }) },
 }
 
 return config
