db8a7e4997c8e1e786ac004bef23218841ca8b5e
Oliver Hofkens <23633993+OliverHofkens@users.noreply.github.com>
2026-05-22 10:31:49 +0200

    feat: follow system theme in wezterm and neovim

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,36 @@
 return {
-  "sainnhe/sonokai",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.g.sonokai_enable_italic = true
-    vim.g.sonokai_transparent_background = true
-    vim.g.sonokai_dim_inactive_windows = true
-    vim.cmd.colorscheme("sonokai")
-  end,
+  {
+    "sainnhe/sonokai",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.sonokai_enable_italic = true
+      vim.g.sonokai_transparent_background = true
+      vim.g.sonokai_dim_inactive_windows = true
+      vim.cmd.colorscheme("sonokai")
+    end,
+  },
+  {
+    "sainnhe/everforest",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.everforest_enable_italic = true
+      vim.g.everforest_transparent_background = true
+      vim.g.everforest_dim_inactive_windows = true
+    end,
+  },
+  {
+    "f-person/auto-dark-mode.nvim",
+    opts = {
+      set_dark_mode = function()
+        vim.api.nvim_set_option_value("background", "dark", {})
+        vim.cmd.colorscheme("sonokai")
+      end,
+      set_light_mode = function()
+        vim.api.nvim_set_option_value("background", "light", {})
+        vim.cmd.colorscheme("everforest")
+      end,
+    },
+  },
 }
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -7,8 +7,18 @@ config.check_for_updates = false
 config.max_fps = 120 -- Smoother scrolling in editors
 
 -- GUI
-config.color_scheme = "flexoki-dark"
-config.font = wezterm.font_with_fallback({ "Inconsolata Nerd Font", "unscii" })
+function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "flexoki-dark"
+	else
+		return "flexoki-light"
+	end
+end
+
+config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+
+-- config.font = wezterm.font_with_fallback({ "Inconsolata Nerd Font", "unscii" })
+config.font = wezterm.font_with_fallback({ "Greybeard 16px", "unscii" })
 config.font_size = 16
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
